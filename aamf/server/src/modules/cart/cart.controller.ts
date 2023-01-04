ed7315a05fb4293227bc03193d2242daf9328095
Constantin Câmpean <mail.la.costi@gmail.com>
2023-01-04 12:19:17 +0200

    retrieve the cart items

--- aamf/server/src/modules/cart/cart.controller.ts
+++ aamf/server/src/modules/cart/cart.controller.ts
@@ -1,13 +1,36 @@
 import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../../auth/jwt-auth.guard';
 
+import products from 'src/products';
+
+import type { Product } from 'src/products';
+
+interface CartItem extends Product {
+  quantity: number;
+}
+
+interface Cart {
+  cartItems: CartItem[];
+}
+
+const initialCart = (indexes: number[]): Cart => ({
+  cartItems: indexes.map((index) => ({
+    ...products[index],
+    quantity: 1,
+  })),
+});
+
 @Controller('cart')
 export class CartController {
+  private carts: Record<number, Cart> = {
+    1: initialCart([0, 2, 4]),
+    2: initialCart([1, 3]),
+  };
   constructor() {}
 
   @Get()
   @UseGuards(JwtAuthGuard)
-  async index(@Request() req): Promise<{ userId: number }> {
-    return { userId: req.user.userId };
+  async index(@Request() req): Promise<Cart> {
+    return this.carts[req.user.userId] ?? { cartItems: [] };
   }
 }
